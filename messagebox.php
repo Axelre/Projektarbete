4220ab5beb1b691cf17b361ff179acb32f3560ca
ludvigstrandell <ludvig.strandell@protonmail.com>
2020-05-21 14:07:42 +0200

    fixat saker jag trodde funka som ej gjorde det med messagebox

--- messagebox.php
+++ messagebox.php
@@ -5,44 +5,43 @@
     <title>Messagebox</title>
 </head>
 <body>
+
     <?php
 
-    echo "<form action='Pendingtrades.html' method='POST'> <input type='submit' name='goback' value='Go back'>
-    </form>";
 
-     include("db.php");
-     $sql = "SELECT * from messagebox ORDER BY ID DESC";
-     $stmt = $db->query($sql);
-     $stmt->execute();
-     $stmt->setFetchMode(PDO::FETCH_ASSOC);
-     while($row = $stmt->fetch())
-     {
+date_default_timezone_set('Europe/Stockholm');
 
-     echo "<div>
-             <b>#</b>".$row['ID']."
-             ".$row['Date']."
-             <hr noshade>
-             ".$row['Message']."
-             <hr noshade>
-              </div>";
-     }
+if(isset($_POST['submitBtn'])){
+    $message = $_POST['Message'];
+    $date = date('Y-m-d H:i:s');    
+    include("db.php");
+    $sql = "INSERT INTO messagebox (message, date) VALUES ('$message', '$date')";
+    $db->query($sql);
+    
 
+}
 
-     date_default_timezone_set('Europe/Stockholm');
-
-     if(isset($_POST['submitBtn'])){
-         $message = $_POST['Message'];
-         $date = date('Y-m-d H:i:s');    
-         include("db.php");
-         $sql = "INSERT INTO messagebox (message, date) VALUES ('$message', '$date')";
-         $db->query($sql);
-
-     }
-
-
+    include("db.php");
+    $sql = "SELECT * from messagebox ORDER BY ID DESC";
+    $stmt = $db->query($sql);
+    $stmt->execute();
+    $stmt->setFetchMode(PDO::FETCH_ASSOC);
+    while($row = $stmt->fetch())
+    {
+    
+    echo "<div>
+            <b>#</b>".$row['ID']."
+            ".$row['Date']."
+            <hr noshade>
+            ".$row['Message']."
+            <hr noshade>
+             </div>";
+    }
+         
      ?>
-
-
+     <form action="Pendingtrades.html">
+    <input type="submit" value="Go back"/>
+</form>    
 </body>
 </html>
 
